d44a8651f21efd3f3be18f17874937603dee39d9
Jose Ugal <joseugal17@icloud.com>
2025-11-13 16:38:52 +0100

    feat(app): register services from App and Infrastructure assemblies at startup
    - Added service installation via InstallServicesFromAssemblies in Program.cs.
    - Automatically registers all IServiceInstaller implementations from App and Infrastructure assemblies.
    - Simplifies dependency injection setup and improves modularity.

---
+++ App/AssemblyReference.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+namespace App;
+
+/// <summary>
+/// Represents the app assembly reference.
+/// </summary>
+public static class AssemblyReference
+{
+    /// <summary>
+    /// The assembly.
+    /// </summary>
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,13 @@
+using App;
+using Infrastructure.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.InstallServicesFromAssemblies(
+    builder.Configuration,
+    AssemblyReference.Assembly,
+    Infrastructure.AssemblyReference.Assembly
+);
 
 WebApplication app = builder.Build();
 
